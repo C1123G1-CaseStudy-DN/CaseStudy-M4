02b596c4f886ca72b7f8ca2cb0cf261ac8cc2f9d
luugiahuy123 <luugiahuyqn@gmail.com>
2024-04-26 15:22:28 +0700

    test

--- blog/src/main/java/com/example/blog/repository/user/IUserRepository.java
+++ blog/src/main/java/com/example/blog/repository/user/IUserRepository.java
@@ -1,4 +1,7 @@
 package com.example.blog.repository.user;
 
-public interface IUserRepository {
+import com.example.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<User , Integer> {
 }
